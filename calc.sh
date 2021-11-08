eef1ac080d3b6a9c8d26ef427be2d2f8d9b115f4
samikshyakc1996 <samikshyakc1996@gmail.com>
2021-11-07 23:44:21 -0500

    greeting messgae

--- calc.sh
+++ calc.sh
@@ -9,3 +9,6 @@
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
+echo "Hello, Welcome to the Calculator Program"
+
+
