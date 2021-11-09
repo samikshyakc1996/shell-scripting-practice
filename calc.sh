80fb8759ca4230f18f2764092a26e9d5d7b76ffe
samikshyakc1996 <samikshyakc1996@gmail.com>
2021-11-09 13:23:01 -0500

    Indentation

--- calc.sh
+++ calc.sh
@@ -12,40 +12,40 @@
 echo "Hello, Welcome to the Calculator Program."
 
 while true
-do
-echo "Which operations would you like to prefer? Your choices are:
-1) Addition (Use these keywords: Add, add, +, 1) 
-2) Subtraction (Use these keywords: Subtract, sub, -, Difference, diff, 2) 
-3) Exit, exit, 3 "
+  do
+    echo "Which operations would you like to prefer? Your choices are:
+      1) Addition (Use these keywords: Add, add, +, 1) 
+      2) Subtraction (Use these keywords: Subtract, sub, -, Difference, diff, 2) 
+      3) Exit, exit, 3 "
 
-read OPERATION
-case $OPERATION in 
-"Add" | "+" | "add"| "1")
-echo "Enter your first number" 
-read NUM1
-echo "Enter your second number" 
-read NUM2
+    read OPERATION
+    case $OPERATION in 
+    "Add" | "+" | "add"| "1")
+    echo "Enter your first number" 
+    read NUM1
+    echo "Enter your second number" 
+    read NUM2
 
-echo "The Sum is:" 
-RESULT= expr $NUM1 + $NUM2 
+    echo "The Sum is:" 
+    RESULT= expr $NUM1 + $NUM2 
 
-;;
-"Subtract" | "-" | "Sub" | "sub"|"Difference" |"diff"| "2" )
-echo "Enter your first number" 
-read NUM1
-echo "Enter your second number" 
-read NUM2
+    ;;
+    "Subtract" | "-" | "Sub" | "sub"|"Difference" |"diff"| "2" )
+    echo "Enter your first number" 
+    read NUM1
+    echo "Enter your second number" 
+    read NUM2
 
-echo "The difference is:"
-RESULT= expr $NUM1 - $NUM2
-;;
-"Exit"|"exit" |"3")
-break
-;;
+    echo "The difference is:"
+    RESULT= expr $NUM1 - $NUM2
+    ;;
+    "Exit"|"exit" |"3")
+    break
+    ;;
 
-*)
-echo "Sorry, your choice did not match any operation."
-esac
-done
+    *)
+    echo "Sorry, your choice did not match any operation."
+    esac
+  done
 
 
