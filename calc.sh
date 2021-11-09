bcf8edbd85135e3a0e1602af0e6fc20268651a84
samikshyakc1996 <samikshyakc1996@gmail.com>
2021-11-09 13:18:27 -0500

    added Subtraction, exit operation and also added infinite loop

--- calc.sh
+++ calc.sh
@@ -9,18 +9,43 @@
 # If the selection matches a supported operation, execute the operation.
 # If the selection does not match a support operation, display an error message.
 # When the operation is complete, redisplay the menu.
-echo "Hello, Welcome to the Calculator Program"
-echo "Which operations would you like to prefer? #Add, #Subtract or #Exit"
+echo "Hello, Welcome to the Calculator Program."
+
+while true
+do
+echo "Which operations would you like to prefer? Your choices are:
+1) Addition (Use these keywords: Add, add, +, 1) 
+2) Subtraction (Use these keywords: Subtract, sub, -, Difference, diff, 2) 
+3) Exit, exit, 3 "
+
 read OPERATION
 case $OPERATION in 
-"Add")
-echo "what are your numbers" 
+"Add" | "+" | "add"| "1")
+echo "Enter your first number" 
 read NUM1
+echo "Enter your second number" 
 read NUM2
-RESULT= expr $NUM1 + $NUM2
-echo $RESULT
+
+echo "The Sum is:" 
+RESULT= expr $NUM1 + $NUM2 
 
 ;;
+"Subtract" | "-" | "Sub" | "sub"|"Difference" |"diff"| "2" )
+echo "Enter your first number" 
+read NUM1
+echo "Enter your second number" 
+read NUM2
+
+echo "The difference is:"
+RESULT= expr $NUM1 - $NUM2
+;;
+"Exit"|"exit" |"3")
+break
+;;
+
+*)
+echo "Sorry, your choice did not match any operation."
 esac
+done
 
 
